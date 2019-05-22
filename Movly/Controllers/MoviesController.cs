b1f792a09d928f1339c5fc1593ce55b2cd6d4449
Yesid Perea <yesid.perea@outlook.com>
2019-05-21 21:58:32 -0500

    Adding Movie POCO, MoviesController and Random view.

---
+++ Movly/Controllers/MoviesController.cs
@@ -0,0 +1,17 @@
+﻿using Movly.Models;
+using System.Web.Mvc;
+
+namespace Movly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+
+            Movie movie = new Movie() {Name = "Shrek"};
+
+            return View(movie);
+        }
+    }
+}
